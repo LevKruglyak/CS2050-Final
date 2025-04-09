523ae75d54b5df2f858c7bb24aec57e65ca99e31
LevKruglyak <lev.kruglyak2014@gmail.com>
2025-04-09 16:29:13 -0400

    switch backend and rewrite stu

--- src/core/simulation.h
+++ src/core/simulation.h
@@ -5,91 +5,118 @@
 #include <fmt/core.h>
 #include <fmt/format.h>
 #include <glm/glm.hpp>
-#include <highfive/highfive.hpp>
-#include <iostream>
 #include <omp.h>
 #include <vector>
 
-using vec3 = glm::dvec3;
+using vec2 = glm::dvec2;
 
 struct Particle {
-  vec3 pos = vec3();
-  vec3 vel = vec3();
-  vec3 acc = vec3();
+  vec2 pos = vec2();
+  vec2 vel = vec2();
+  vec2 acc = vec2();
   double mass = 0.0;
-};
-
-struct SimulationParams {
-  double grav_constant;
-  double softening;
-  double dt;
-  double radius;
 };
 
 class Simulation {
 public:
+  int resolution;
+  size_t N;
+  double total_mass;
+
+  double grav_constant = 0.01;
+  double softening = 0.001;
+  double dt = 0.01;
+  double radius = 10.0;
+  double particles_per_cell = 10;
+
   std::vector<Particle> particles;
+  std::vector<double> density;
 
-  std::vector<double> es;
-  std::vector<double> kes;
-  std::vector<double> pes;
-
-  SimulationParams params;
-
-  inline vec3 periodic_wrap(vec3 v) {
-    return v - glm::round(v / params.radius) * params.radius;
+  inline vec2 periodic_wrap(vec2 v) {
+    return v - glm::round(v / radius) * radius;
   }
 
-  inline vec3 accel(vec3 target, vec3 position) {
-    vec3 diff = periodic_wrap(target - position);
+  inline vec2 accel(vec2 target, vec2 position) {
+    vec2 diff = periodic_wrap(target - position);
 
-    double dist2 = glm::dot(diff, diff) + params.softening * params.softening;
+    double dist2 = glm::dot(diff, diff) + softening * softening;
     double inv_r3 = 1.0 / (dist2 * std::sqrt(dist2));
 
-    return params.grav_constant * diff * inv_r3;
+    return grav_constant * diff * inv_r3;
   }
 
-  Simulation(std::vector<Particle> particles, SimulationParams params)
-      : particles(particles), params(params) {}
+  Simulation(std::vector<Particle> particles) : particles(particles) {
+    N = particles.size();
+    resolution = std::ceil(sqrt((double)N / particles_per_cell));
+    density.resize(resolution * resolution, 0);
+    total_mass = 0.0;
+    for (const auto &particle : particles) {
+      total_mass += particle.mass;
+    }
+    updateDensity();
+  }
+
+  void updateDensity() {
+    // Update density
+    std::fill(density.begin(), density.end(), 0.0f);
+    for (size_t i = 0; i < particles.size(); i++) {
+      float dX = (particles[i].pos.x / radius + 0.5) * resolution;
+      float dY = (particles[i].pos.y / radius + 0.5) * resolution;
+      int gridX = (int)dX;
+      int gridY = (int)dY;
+      dX -= gridX;
+      dY -= gridY;
+
+      auto wrapIndex = [&](int i) {
+        i = i % resolution;
+        if (i < 0)
+          i += resolution;
+        return i;
+      };
+
+      int nX = wrapIndex(dX < 0.5 ? gridX - 1 : gridX + 1);
+      int nY = wrapIndex(dY < 0.5 ? gridY - 1 : gridY + 1);
+
+      float oX = abs(0.5f - dX);
+      float oY = abs(0.5f - dY);
+      float weight00 = (1.0f - oX) * (1.0f - oY);
+      float weight10 = oX * (1.0f - oY);
+      float weight01 = (1.0f - oX) * oY;
+      float weight11 = oX * oY;
+
+      density[gridY * resolution + gridX] += weight00 * particles[i].mass;
+      density[gridY * resolution + nX] += weight10 * particles[i].mass;
+      density[nY * resolution + gridX] += weight01 * particles[i].mass;
+      density[nY * resolution + nX] += weight11 * particles[i].mass;
+    }
+  }
 
   void update() {
 #pragma omp parallel for schedule(dynamic)
-    for (uint i = 0; i < particles.size(); i++) {
-      vec3 p = particles[i].pos;
-      vec3 v = particles[i].vel;
-      vec3 a = particles[i].acc;
+    for (uint i = 0; i < N; i++) {
+      vec2 p = particles[i].pos;
+      vec2 v = particles[i].vel;
+      vec2 a = particles[i].acc;
 
       // Update position
-      vec3 np = p + v * params.dt + a * (params.dt * params.dt * 0.5);
+      vec2 np = p + v * dt + a * (dt * dt * 0.5);
 
       // Calculate forces
-      vec3 na = vec3();
-#pragma omp parallel for reduction(+ : na)
-      for (uint j = 0; j < particles.size(); j++) {
+      vec2 na = vec2();
+      // #pragma omp parallel for reduction(+ : na)
+      for (uint j = 0; j < N; j++) {
         na +=
-            (i != j) ? particles[j].mass * accel(particles[j].pos, np) : vec3();
+            (i != j) ? particles[j].mass * accel(particles[j].pos, np) : vec2();
       }
 
-      particles[i].pos = periodic_wrap(np);
-      particles[i].vel = v + (a + na) * (params.dt * 0.5);
-      particles[i].acc = na;
+      particles[i] = Particle{
+          .pos = periodic_wrap(np),
+          .vel = v + (a + na) * (dt * 0.5),
+          .acc = na,
+          .mass = particles[i].mass,
+      };
     }
 
-    // Calculate total energy
-    double ke = 0.0;
-    double pe = 0.0;
-#pragma omp parallel for reduction(+ : ke, pe)
-    for (uint i = 0; i < particles.size(); i++) {
-      vec3 v = particles[i].vel;
-      vec3 a = particles[i].acc;
-      double m = particles[i].mass;
-
-      ke += 0.5 * m * glm::dot(v, v);
-      pe -= 0.5 * m * glm::dot(a, a);
-    }
-
-    kes.push_back(ke);
-    pes.push_back(pe);
-    es.push_back(ke + pe);
+    updateDensity();
   }
 };
--- src/gui/main_app.cpp
+++ src/gui/main_app.cpp
@@ -1,154 +1,174 @@
-#include "glm/gtc/constants.hpp"
 #include "imgui.h"
-#include "imgui_app.h"
-#include "implot.h"
-#include "implot_internal.h"
+#include "imgui_internal.h"
+#include "implot/implot.h"
+#include "implot/implot_internal.h"
 #include "simulation.h"
-#include <memory>
+
+#include "immapp/runner.h"
+#include <iostream>
 #include <random>
 
-class GravitySimulator : public ImGuiApp {
-  std::unique_ptr<Simulation> simulation;
-  bool running = false;
+#include "hello_imgui/hello_imgui_include_opengl.h"
+#include "imgui.h"
+#include "simulation.h"
+
+class GravitySimulator {
+  std::unique_ptr<Simulation> sim;
+
+  std::vector<double> x_data = {};
+  std::vector<double> y_data = {};
+  bool updated = false;
+
+  std::vector<float> density = {};
+  GLuint densityTexture;
+
+  ImVec4 particle_color = ImVec4(1.0, 1.0, 1.0, 1.0);
+  float particle_radius = 0.5;
+  ImVec4 bounds_color = ImVec4(1.0, 1.0, 1.0, 1.0);
 
 public:
   GravitySimulator() {
-    auto specs = AppSpecs();
-    specs.name = "Newtonian Gravity Simulator";
-    specs.width = 1000;
-    specs.height = 1000;
-
-    create(specs);
-
     std::random_device rd;
     std::mt19937 rng(rd());
 
-    // std::uniform_real_distribution<double> angle_sample(0.0,
-    //                                                     glm::tau<double>());
-    // std::uniform_real_distribution<double> radius_sample(4.0, 6.0);
-    //
-    // double black_hole_mass = 10.0;
-    //
-    // std::vector<particle> particles;
-    // const uint num_particles = 5000;
-    //
-    // particles.push_back(particle{.mass = black_hole_mass});
-    //
-    // for (uint i = 0; i < num_particles; i++) {
-    //   double radius = radius_sample(rng);
-    //   double angle = angle_sample(rng);
-    //
-    //   double x = cos(angle);
-    //   double y = sin(angle);
-    //   double speed = sqrt(GRAV * black_hole_mass / radius);
-    //
-    //   particles.push_back(particle{.pos = radius * vec3(x, y, 0.0),
-    //                                .vel = speed * vec3(-y, x, 0.0),
-    //                                .mass = 0.00000000001});
-    // }
-    SimulationParams params = SimulationParams{
-        .grav_constant = 0.01,
-        .softening = 0.001,
-        .dt = 0.01,
-        .radius = 10.0,
-    };
-
     std::vector<Particle> particles;
-    const double total_mass = 1.0;
-    const uint num_particles = 5000;
+    const double total_mass = 10.0;
+    const uint num_particles = 100000;
 
     std::uniform_real_distribution<double> sample(-5.0, 5.0);
 
     for (uint i = 0; i < num_particles; i++) {
-      while (true) {
-        vec3 x = vec3(sample(rng), sample(rng), 0.0);
+      vec2 x = vec2(sample(rng), sample(rng));
 
-        // if (dot(x, x) < 1.0) {
-        particles.push_back(
-            Particle{.pos = x, .mass = total_mass / num_particles});
-        break;
-        // }
-      }
+      particles.push_back(
+          Particle{.pos = x, .mass = total_mass / num_particles});
     }
 
-    simulation = std::make_unique<Simulation>(particles, params);
+    sim = std::make_unique<Simulation>(particles);
+    density.resize(sim->resolution * sim->resolution, 0.0);
   }
 
-  ~GravitySimulator() { destroy(); }
+  bool running = false;
 
-  void update() override {
+  void init() {
+    glGenTextures(1, &densityTexture);
+    glBindTexture(GL_TEXTURE_2D, densityTexture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_R32F, sim->resolution, sim->resolution, 0,
+                 GL_RED, GL_FLOAT, density.data());
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_R, GL_RED);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_G, GL_RED);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_B, GL_RED);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_A, GL_ONE);
+  }
+
+  void update() {
     auto io = ImGui::GetIO();
 
-    std::vector<double> x_data = {};
-    std::vector<double> y_data = {};
+    ImGui::Columns(2, "viewport", false);
+    ImGui::SetColumnWidth(0, ImGui::GetIO().DisplaySize.x * 0.7);
 
-    for (const auto &p : simulation->particles) {
-      x_data.push_back(p.pos.x);
-      y_data.push_back(p.pos.y);
+    if (!updated) {
+      x_data.reserve(sim->N);
+      y_data.reserve(sim->N);
+      for (uint i = 0; i < sim->N; i++) {
+        x_data[i] = sim->particles[i].pos.x;
+        y_data[i] = sim->particles[i].pos.y;
+      }
+
+      for (uint i = 0; i < sim->resolution * sim->resolution; i++) {
+        density[i] = (sim->density[i] / sim->total_mass) *
+                     (sim->resolution * sim->resolution) * 0.2;
+      }
+
+      glBindTexture(GL_TEXTURE_2D, densityTexture);
+      glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, sim->resolution, sim->resolution,
+                      GL_RED, GL_FLOAT, density.data());
+      updated = true;
     }
 
-    {
-      ImGui::SetNextWindowPos(ImVec2(0, 0));
-      ImGui::SetNextWindowSize(ImGui::GetIO().DisplaySize);
-      ImGui::Begin("BackPanel", nullptr,
-                   ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize |
-                       ImGuiWindowFlags_NoMove |
-                       ImGuiWindowFlags_NoBringToFrontOnFocus |
-                       ImGuiWindowFlags_NoNav);
+    if (ImPlot::BeginPlot("Viewport", ImVec2(-1.0, -1.0), ImPlotFlags_Equal)) {
+      ImPlot::SetupAxes("x [Mpc]", "y [Mpc]");
+      ImPlot::GetStyle().MarkerSize = particle_radius;
+
+      float hr = sim->radius / 2;
+      ImPlot::PlotImage("density", densityTexture, ImPlotPoint(-hr, hr),
+                        ImPlotPoint(hr, -hr));
+      ImPlot::SetNextLineStyle(particle_color);
+      ImPlot::PlotScatter("particles", x_data.data(), y_data.data(), sim->N);
+
+      ImPlot::SetNextFillStyle(bounds_color);
+      if (ImPlot::BeginItem("bounds", 0)) {
+        ImDrawList *draw_list = ImPlot::GetPlotDrawList();
+        ImVec2 p_min = ImPlot::PlotToPixels(ImPlotPoint(-hr, -hr));
+        ImVec2 p_max = ImPlot::PlotToPixels(ImPlotPoint(hr, hr));
+        draw_list->AddRect(p_min, p_max, ImPlot::GetCurrentItem()->Color, 0.0f,
+                           0, 2.0f);
+        ImPlot::EndItem();
+      }
+
+      ImPlot::EndPlot();
+      ImGui::NextColumn();
+
+      ImGui::Separator();
+      if (ImGui::CollapsingHeader("Viewport Settings")) {
+        ImGui::ColorEdit4("Particle Color", (float *)&particle_color);
+        ImGui::SliderFloat("Particle Radius", &particle_radius, 0.01, 10.0);
+        ImGui::ColorEdit4("Bounds Color", (float *)&bounds_color);
+      }
+
+      ImGui::Separator();
+      ImGui::Text("Number of Particles: %d", (int)sim->N);
+      ImGui::Text("softening: %f", sim->softening);
+      ImGui::Text("dt: %f", sim->dt);
+      ImGui::Text("G: %f", sim->grav_constant);
+      ImGui::Text("R: %f", sim->radius);
+      ImGui::Text("total mass: %f", sim->total_mass);
+      ImGui::Text("resolution: %d", sim->resolution);
+
+      ImGui::Separator();
 
       ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
                   1000.0f / io.Framerate, io.Framerate);
       ImGui::Checkbox("Running", &running);
 
-      ImGui::Columns(2, "simulation", false);
-      ImGui::SetColumnWidth(0, ImGui::GetIO().DisplaySize.x * 0.5);
-      if (ImPlot::BeginPlot("Viewport", ImVec2(-1.0, -1.0),
-                            ImPlotFlags_Equal)) {
-        ImPlot::SetupAxes("x [Mpc]", "y [Mpc]");
-        ImPlot::GetStyle().MarkerSize = 1;
-        ImPlot::GetStyle().FillAlpha = 0.8;
-        ImPlot::PlotScatter("particles", x_data.data(), y_data.data(),
-                            simulation->particles.size());
-
-        if (ImPlot::BeginItem("bounds")) {
-          float hr = simulation->params.radius / 2;
-          ImVec2 p_min = ImPlot::PlotToPixels(ImPlotPoint(-hr, -hr));
-          ImVec2 p_max = ImPlot::PlotToPixels(ImPlotPoint(hr, hr));
-          ImDrawList *draw_list = ImPlot::GetPlotDrawList();
-          draw_list->AddRect(p_min, p_max, ImPlot::GetCurrentItem()->Color,
-                             0.0f, 0, 2.0f);
-          ImPlot::EndItem();
-        }
-
-        ImPlot::EndPlot();
-      }
-      ImGui::NextColumn();
-
-      if (ImPlot::BeginPlot("Energy", ImVec2(-1.0, 0.0))) {
-        ImPlot::SetupAxes("Time [s]", "Energy", ImPlotAxisFlags_AutoFit,
-                          ImPlotAxisFlags_AutoFit);
-        ImPlot::SetupLegend(ImPlotLocation_NorthEast);
-        ImPlot::PlotLine("total", simulation->es.data(), simulation->es.size(),
-                         simulation->params.dt);
-        ImPlot::PlotLine("kinetic", simulation->kes.data(),
-                         simulation->kes.size(), simulation->params.dt);
-        ImPlot::PlotLine("potential", simulation->pes.data(),
-                         simulation->pes.size(), simulation->params.dt);
-        ImPlot::EndPlot();
-      }
       ImGui::EndColumns();
-
-      ImGui::End();
     }
 
     if (running) {
-      simulation->update();
+      sim->update();
+      updated = false;
     }
   }
 };
 
-// Main code
+void showWindow(std::string name) {
+  auto windowPtr =
+      HelloImGui::GetRunnerParams()->dockingParams.dockableWindowOfName(name);
+  if (windowPtr) {
+    windowPtr->isVisible = true;
+  }
+}
+
 int main(int, char **) {
   GravitySimulator simulator;
-  simulator.run();
+
+  HelloImGui::RunnerParams runnerParams;
+  runnerParams.appWindowParams.windowTitle =
+      "2D Cosmological Structure Formation Simulator";
+  runnerParams.imGuiWindowParams.menuAppTitle = "Main";
+  runnerParams.appWindowParams.windowGeometry.size = {1920, 1080};
+  runnerParams.imGuiWindowParams.showMenuBar = true;
+  runnerParams.imGuiWindowParams.showStatusBar = false;
+  runnerParams.fpsIdling.enableIdling = false;
+  runnerParams.callbacks.PostInit = [&simulator]() { simulator.init(); };
+  runnerParams.callbacks.ShowGui = [&simulator]() { simulator.update(); };
+
+  ImmApp::AddOnsParams addOnsParams;
+  addOnsParams.withImplot = true;
+
+  ImmApp::Run(runnerParams, addOnsParams);
+
+  return 0;
 }
