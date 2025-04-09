bcda3c985577f0828808496e7c9fb7b96f7f14ba
LevKruglyak <lev.kruglyak2014@gmail.com>
2025-04-09 19:43:43 -0400

    prepare for run

--- src/cli/main_cli.cpp
+++ src/cli/main_cli.cpp
@@ -24,15 +24,15 @@ int main(int argc, char *argv[]) {
   (void)argc;
   (void)argv;
 
-  auto sim = std::make_unique<Simulation>(10.0, 20.0, 100000);
+  auto sim = std::make_unique<Simulation>(10.0, 20.0, 1000000);
 
   printf("loaded simulation\n");
   for (int i = 0; i < 10000; i++) {
     sim->update();
 
-    if (i % 10 == 0) {
+    if (i % 100 == 0) {
       std::ostringstream oss;
-      oss << argv[1] << "_" << std::setw(2) << std::setfill('0') << i / 10
+      oss << argv[1] << "_" << std::setw(2) << std::setfill('0') << i / 100
           << ".png";
       std::vector<double> density(sim->density.size());
       for (uint i = 0; i < sim->resolution * sim->resolution; i++) {
